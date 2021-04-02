6b690d7c5e71f878c5a58db882d72cbf528a5f2b
Basma-Elsaify <basmaelsaify@gmail.com>
2021-04-02 21:20:35 +0400

    Update custom.py
    updated custom example

--- contrib/opencensus-ext-fastapi/examples/custom.py
+++ contrib/opencensus-ext-fastapi/examples/custom.py
@@ -1,17 +1,3 @@
-# Copyright 2017, OpenCensus Authors
-#
-# Licensed under the Apache License, Version 2.0 (the "License");
-# you may not use this file except in compliance with the License.
-# You may obtain a copy of the License at
-#
-#     http://www.apache.org/licenses/LICENSE-2.0
-#
-# Unless required by applicable law or agreed to in writing, software
-# distributed under the License is distributed on an "AS IS" BASIS,
-# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-# See the License for the specific language governing permissions and
-# limitations under the License.
-
 import os
 
 from fastapi import FastAPI, Request
@@ -29,51 +15,26 @@ from opencensus.trace import config_integration, samplers
 from opencensus.ext.azure.trace_exporter import AzureExporter
 from opencensus.trace.samplers import ProbabilitySampler
 
-INTEGRATIONS = ['mysql', 'logging']
-
-DB_HOST = '127.0.0.1'
-
-PROJECT = os.environ.get('GCLOUD_PROJECT_PYTHON')
-
-# MySQL settings
-MYSQL_PASSWORD = os.environ.get('SYSTEST_MYSQL_PASSWORD')
-
-# hello_world_server location
-HELLO_WORLD_HOST_PORT = 'localhost:50051'
-
 app = FastAPI()
 
-# optional TODO: test with stack driver
+@app.on_event("startup")
+async def startup_event():
+    app.trace_exporter = AzureExporter(connection_string=f'InstrumentationKey=<InstrumentationKey>')
 
-# Enable tracing, configure the trace params, send traces to Azure Trace
-exporter = AzureExporter(connection_string="InstrumentationKey=<InstrumentationKey>")
-sampler = ProbabilitySampler(rate=1.0)
+app.add_middleware(FastAPIMiddleware)
 
-middleware = FastAPIMiddleware(
-    app,
-    exporter=exporter,
-    sampler=sampler,
-)
+@app.get("/")
+async def root():
+    return "Hello World!"
 
-config_integration.trace_integrations(INTEGRATIONS)
+@app.get("/items/{item_id}")
+async def read_root(item_id: str, request: Request):
+    client_host = request.client.host
+    return {"client_host": client_host, "item_id": item_id}
 
-# cache of (stub_cls, hostport) -> grpc stub
-_stub_cache = {}
-
-
-@app.route('/')
-def hello():
-    return 'Hello world!'
-
-# optional
-@app.route('/mysql')
-def mysql_query():
-    try:
-        return None
-
-    except Exception:
-        msg = "Query failed. Check your env vars for connection settings."
-        return None
+@app.post("/name/")
+async def send_name(name: str):
+    return {'name': name}
 
 if __name__ == '__main__':
-    uvicorn.run("custom:app", port=8888, reload=True, debug=True, workers=3)
+    uvicorn.run("custom:app", host="127.0.0.1", port=8888, log_level="info")
